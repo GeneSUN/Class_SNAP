1e9aa7f18dd7914c220829a75d6d9d7e237112e2
Zhe Sun <injure21@gmail.com>
2023-12-06 09:39:01 -0500

    worked

--- pre_enodeb.py
+++ pre_enodeb.py
@@ -21,7 +21,7 @@ import concurrent.futures
 import sys 
 sys.path.append('/usr/apps/vmas/script/ZS/SNAP') 
 from class_SNAP import SNAP_pre_enodeb, get_date_window
-
+from MailSender import MailSender
 from hdfs import InsecureClient 
 
 def pre_enodeb(spark, date_str, sourse_path, path_list, id_column):
@@ -50,17 +50,50 @@ if __name__ == "__main__":
             .master("spark://njbbepapa1.nss.vzwnet.com:7077") \
             .config("spark.sql.adapative.enabled","true")\
             .enableHiveSupport().getOrCreate()
-    parser = argparse.ArgumentParser(description="Inputs for generating Post SNA Maintenance Script Trial")
+    parser = argparse.ArgumentParser(description="Inputs") 
+    parser.add_argument("--date", default=datetime.today().strftime('%Y-%m-%d')) 
+    args = parser.parse_args()
+    date_str = args.date
+    if date_str == "no date input from shell script":
+        date_str = (date.today() - timedelta(1) ).strftime("%Y-%m-%d")
 
+    mail_sender = MailSender() 
+    mail_sender.send(text = date_str)
+    """
+    try:
+        date_str = (date.today() - timedelta(1) ).strftime("%Y-%m-%d")
+        #date_str = "2023-11-30"
+        hdfs_title = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000/'
+        sourse_path = hdfs_title + "/user/rohitkovvuri/nokia_fsm_kpis_updated_v3/NokiaFSMKPIsSNAP_{}.csv"
+        path_list = ["/user/ZheS/MonitorEnodebPef/enodeb/Event_Enodeb_List_Date/Event_Enodeb_List_{}.csv",
+                    "/user/ZheS/MonitorEnodebPef/enodeb//Daily_KPI_14_days_pre_Event/Daily_KPI_14_days_pre_{}.csv",
+                    "/user/ZheS/MonitorEnodebPef/enodeb/Event_Enodeb_Pre_Feature_Date/Event_Enodeb_Pre_{}.csv"]
+        path_list = [ hdfs_title + path.format(date_str) for path in path_list]
+        id_column = ['ENODEB']
+
+        pre_enodeb(spark,date_str,sourse_path, path_list, id_column)
+
+        df_kpis = spark.read.option("header", "true").csv( path_list[0] )
+
+    except Exception as e:
+        print(e)
+        mail_sender = MailSender() 
+        mail_sender.send(send_from="sassupport@verizon.com", 
+        send_to=["zhe.sun@verizonwireless.com"], 
+        subject="insufficient Enodeb data", 
+        cc=[], 
+        text=f"an error occured at pre_enodeb code: {e}") 
     
-    date_str = (date.today() - timedelta(1) ).strftime("%Y-%m-%d")
-    date_str = "2023-11-30"
-    hdfs_title = 'hdfs://njbbvmaspd11.nss.vzwnet.com:9000/'
-    sourse_path = hdfs_title + "/user/rohitkovvuri/nokia_fsm_kpis_updated_v3/NokiaFSMKPIsSNAP_{}.csv"
-    path_list = ["/user/ZheS/MonitorEnodebPef/enodeb/Event_Enodeb_List_Date/Event_Enodeb_List_{}.csv",
-                "/user/ZheS/MonitorEnodebPef/enodeb//Daily_KPI_14_days_pre_Event/Daily_KPI_14_days_pre_{}.csv",
-                "/user/ZheS/MonitorEnodebPef/enodeb/Event_Enodeb_Pre_Feature_Date/Event_Enodeb_Pre_{}.csv"]
-    path_list = [ hdfs_title + path.format(date_str) for path in path_list]
-    id_column = ['ENODEB']
+    else:
+        
+        if df_kpis.count() < 1000:
+            mail_sender = MailSender() 
+            mail_sender.send(send_from="sassupport@verizon.com", 
+            send_to=["zhe.sun@verizonwireless.com"], 
+            subject="insufficient Enodeb data", 
+            cc=[], 
+            text=f"no enodeb list at {date_str}") 
+    finally:
+        pass
 
-    pre_enodeb(spark,date_str,sourse_path, path_list, id_column)
+    """
--- pre_enodeb.sh
+++ pre_enodeb.sh
@@ -5,7 +5,20 @@ export SPARK_HOME=/usr/apps/vmas/spark-3.2.0-bin-hadoop3.2
 export PYSPARK_PYTHON=/usr/apps/vmas/anaconda3/bin/python3 
 echo "RES: Starting===" 
 echo $(date) 
-/usr/apps/vmas/spark-3.2.0-bin-hadoop3.2/bin/spark-submit --master spark://njbbepapa1.nss.vzwnet.com:7077 --conf "spark.sql.session.timeZone=UTC" --conf "spark.driver.maxResultSize=2g" --conf "spark.dynamicAllocation.enabled=false" --num-executors 10 --executor-cores 2 --total-executor-cores 20 --executor-memory 2g --driver-memory 6g --packages org.apache.spark:spark-avro_2.12:3.2.0,io.streamnative.connectors:pulsar-spark-connector_2.12:3.2.0.2 /usr/apps/vmas/script/ZS/SNAP/enodeb/pre_enodeb.py> /usr/apps/vmas/script/ZS/SNAP/enodeb/pre_enodeb.log
+/usr/apps/vmas/spark-3.2.0-bin-hadoop3.2/bin/spark-submit \
+--master spark://njbbepapa1.nss.vzwnet.com:7077 \
+--conf "spark.sql.session.timeZone=UTC" \
+--conf "spark.driver.maxResultSize=2g" \
+--conf "spark.dynamicAllocation.enabled=false" \
+--num-executors 10 \
+--executor-cores 2 \
+--total-executor-cores 20 \
+--executor-memory 2g \
+--driver-memory 6g \
+--packages org.apache.spark:spark-avro_2.12:3.2.0,io.streamnative.connectors:pulsar-spark-connector_2.12:3.2.0.2 \
+/usr/apps/vmas/script/ZS/SNAP/enodeb/pre_enodeb.py \
+--date "2023-12-04"\
+> /usr/apps/vmas/script/ZS/SNAP/enodeb/pre_enodeb.log
 
  
  
